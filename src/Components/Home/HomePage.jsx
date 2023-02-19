2ad6d23ecf263f72249d8ec9bd042497ea3b853a
ruabsarnul <122334016+ruabsarnul@users.noreply.github.com>
2023-02-19 23:01:50 +0600

    update

---
+++ src/Components/Home/HomePage.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import SubHomePage from './SubHomePage';
+const HomePage = () => {
+
+const [page, setPage] = useState([0])
+
+useEffect(() => {
+fetch()
+.then(result => result.json())
+.then(data => setPage(data))
+
+}), []
+
+
+
+
+    
+    return (
+        <div>
+            {
+
+                page.map(data =>  <SubHomePage setUser="data"></SubHomePage>)
+            }
+            
+            
+           
+        </div>
+    );
+};
+
+export default HomePage;
